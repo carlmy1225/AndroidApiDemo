4c6384ec73117f528f70ce7d46f3b389b4cd682a
xwj解文军 <wjxie@ctrip.com>
2017-09-29 14:31:18 +0800

    模块之间的路由

---
+++ app/src/main/java/com/jwj/demo/androidapidemo/router/IRouter.java
@@ -0,0 +1,15 @@
+package com.jwj.demo.androidapidemo.router;
+
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Description: 描述
+ * Author: wjxie
+ * Date: 2017/9/29
+ * Copyright: Ctrip
+ */
+
+public interface IRouter {
+    void goTo(Context context, String page, Bundle bundle);
+}
